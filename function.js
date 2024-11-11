13003405ca2615fe7bfe353a1f0f4ace4e84d2ae
LedZeppelins <eisenhowerisded@gmail.com>
2024-11-11 12:22:21 +0700

    changes

---
+++ function.js
@@ -0,0 +1,17 @@
+// let a = 12;
+// b = 33;
+// c = a + b;
+// document.write(c)
+// function tambah(a){
+// phi = 22 / 7
+// hasil =  phi * a * a
+// return hasil
+
+// }
+// document.write(tambah(10))
+let p = [["mikro", "makro", "nano"],
+["pika","piko","pako"]]
+for(let i = 0; i < 2;){
+ document.write(p[i]);
+ i++;
+}
--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-let Jumkelas = 7
-let berfungsi = 3
-let diperbaiki = 4
-let i  = 0
 
-while (i <= Jumkelas){
-    if(i <= berfungsi){
-        document.write(`Ruang kelas ${i} berfungsi <br>`);
-        
-    } else if (i == diperbaiki){
-        document.write (`Ruang kelas ${i} sedang diperbaiki <br>`)
-    }
-    else document.write (`Ruang kelas ${i} tidak berfungsi <br>`);
-    i++;
-}
+// let totkel = 7
+// let ruangfungsi = 4
+// let b  = 1
+// while (b <= totkel){
+//     if(b <= ruangfungsi){
+//         document.write(`Ruang kelas ${b} berfungsi <br>`);
+//     } else 
+//         document.write (`Ruang kelas ${b} tidak berfungsi <br>`)
+//     b++;
+// }
+// let Jumkelas = 7
+// let berfungsi = 3
+// let diperbaiki = 4
+// let i  = 1
+// while (i <= Jumkelas){
+//     if(i <= berfungsi){
+//         document.write(`Ruang kelas ${i} berfungsi <br>`);
+//     } else if (i == diperbaiki){
+//         document.write (`Ruang kelas ${i} sedang diperbaiki <br>`)
+//     }
+//     else document.write (`Ruang kelas ${i} tidak berfungsi <br>`);
+//     i++;
+// }
+// Ini yg speaker
 
 // let i = prompt(`Masukkan Nilai Anda: `)
 // if(i >= 85){
@@ -24,3 +34,51 @@ while (i <= Jumkelas){
 // }else {
 //     document.write(`D`)
 // }
+// Ini else if
+
+
+// let i = parseInt(prompt("Pilihlah angka: "));
+// switch(i){
+//     case 1:
+//         document.write (""))
+// }
+// Ini switch case
+
+// let login = prompt("Masukkan username anda: ")
+// if(login === "admin"){
+//     pass = prompt("Masukkan password anda: ")
+//     if(pass === "12345"){
+//         document.write("Login berhasil")
+//     }else
+//         document.write("Password salah");
+// }else 
+// document.write("Username salah!")
+// Ini yang admin
+
+// let pig = "";
+// for (let i = 5; i >= 1; i--){
+//     for(let j = 1; j <= i; j++){
+//         pig += "&#128069"
+//     }
+// pig = `${pig} <br>`
+// }
+// document.write(pig);
+// Ini buat segitiga
+
+// function luaspersegipanjang(a,b){
+//     return a*b;
+// }
+// document.write(luaspersegipanjang(2,4))
+// ini test function
+
+// let b = ['Jawa', 'Sunda', 'Betawi', 'Karawang']
+// document.write(b[1]);
+// Ini test array
+
+// let p = [["mikro", "makro", "nano"],
+// ["pika","piko","pako"]]
+// for(let i = 0; i < 2;){
+//  document.write(p[i]);
+//  i++;
+// }
+// Gabungan array dengan function
